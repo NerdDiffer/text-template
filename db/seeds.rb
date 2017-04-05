f44486dc4f78e69947574b0ef716434a7855437f
Rafael Espinoza <nerddiffer@gmail.com>
2017-04-04 19:17:53 -0700

    Add db seeding from csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+filename = Rails.root.join('db/seeds', 'patients.csv')
+rows = CSV.read(filename, headers: true, encoding: 'UTF-8')
+
+rows.each do |row|
+  user = User.new
+  user.first_name = row['first_name']
+  user.last_name = row['last_name']
+  user.phone_number = row['phone_number']
+  user.email = row['email']
+  user.street_address = row['street_address']
+  user.city = row['city']
+  user.state = row['state']
+  user.zip = row['zip']
+  user.save
+end
