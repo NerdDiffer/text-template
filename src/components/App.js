1ad62d493d255a6d0b1b01c90bf06d672a8ba78c
Rafael Espinoza <nerddiffer@gmail.com>
2017-04-05 22:22:24 -0700

    Add & integrate TemplateForm component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import TemplateForm from './TemplateForm'
+import templatize from '../utils/template'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      interpolated: ''
+    }
+  }
+
+  updateInterpolated = (template, person) => {
+    const interpolated = templatize(template, person)
+    this.setState({ interpolated })
+  }
+
   render() {
     return (
       <div className="App">
+        <TemplateForm update={this.updateInterpolated} />
+        <p>
+          {this.state.interpolated}
+        </p>
       </div>
     );
   }
---
+++ src/components/TemplateForm.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+import { getUsers } from '../utils/api.js'
+
+class TemplateForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      usersById: null, // object
+      userKeys: null, // array
+      // eslint-disable-next-line
+      template: 'Hi there ${first_name} ${last_name}! Is ${phone_number} still the best number to reach you?',
+      selectedId: null // integer, id of the selected user
+    }
+  }
+
+  componentDidMount() {
+    getUsers()
+      .then(res => {
+        const { users } = res
+
+        const userKeys = []
+        for (let key in users[0]) { userKeys.push(key) }
+
+        const usersById = users.reduce((hash, user) => {
+          hash[user.id] = user
+          return hash
+        }, {})
+
+        this.setState({ usersById, userKeys })
+      })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const { template, usersById, selectedId } = this.state
+    const selectedPerson = usersById[selectedId]
+    this.props.update(template, selectedPerson)
+  }
+
+  handleTextAreaChange = e => {
+    const value = e.target.value
+    this.setState({ template: value })
+  }
+
+  handleSelect = e => {
+    const selectedId = e.target.value
+    this.setState({ selectedId })
+  }
+
+  render() {
+    const { usersById, template } = this.state
+
+    return (
+      <form className="template-form" onSubmit={this.handleSubmit}>
+        <textarea value={template} onChange={this.handleTextAreaChange} />
+        {!!usersById ? renderOptions(usersById, this.handleSelect) : null}
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
+}
+
+export default TemplateForm
+
+/* helpers */
+
+function renderOptions(users, cb) {
+  const options = []
+
+  let user, name
+
+  for (let userId in users) {
+    user = users[userId]
+    name = `${user.first_name} ${user.last_name}`
+
+    options.push(
+      <option value={user.id} key={name + ' ' + user.id}>
+        {name}
+      </option>
+    )
+  }
+
+  return (
+    <select onChange={cb}>
+      {options}
+    </select>
+  )
+}
